f541e3aedba61245b7b2b156d9ccf971e34ba89b
Owen Conoly <otconoly@hotmail.com>
2026-07-29 16:39:15 -0400

    implement dfs_fold, define paths

--- src/DFS.v
+++ src/DFS.v
@@ -4,26 +4,55 @@ Import ListNotations.
 
 Section __.
   Context {V : Type}.
+
+  Section path.
+    Context (edge : V -> V -> Prop).
+
+    Fixpoint path (first : V) (p : list V) :=
+      match p with
+      | [] => True
+      | next :: p' => edge first next /\ path next p'
+      end.
+
+    Definition path_to first p last :=
+      path first p /\ last = nth_default first p.
+  End path.
+
   Context {eqbV : V -> V -> bool}.
   Context {graph : map.map V (list V)}.
-  Context {state : Type}.
-  Context (backedge_upd : state -> list V -> V -> V -> state).
-  Context (foreedge_upd : state -> list V -> V -> V -> state).
 
-  Definition set_contains vs v :=
-    List.existsb (eqbV v) vs.
+  Section fold.
+    Context {state : Type}.
+    Context (backedge_upd : state -> list V -> V -> state).
+    Context (foreedge_upd : state -> list V -> V -> state).
 
-  Context (g : graph).
+    Definition set_contains vs v :=
+      List.existsb (eqbV v) vs.
 
-  Definition edges u :=
-    match map.get g u with
-    | Some vs => vs
-    | None => []
-    end.
+    Context (g : graph).
 
-  Fixpoint dfs n vs v :=
-    if set_contains vs v then vs else
-      match n with
-      | S n' => fold_left (dfs n') (v :: vs) (edges v)
-      | O => vs
+    Definition edges u :=
+      match map.get g u with
+      | Some vs => vs
+      | None => []
       end.
+
+    Definition state' : Type := list V * state.
+    Definition backedge_upd' '(vs, st) v := (v :: vs, backedge_upd st vs v).
+    Definition foreedge_upd' '(vs, st) v := (v :: vs, foreedge_upd st vs v).
+
+    Definition already_seen (st' : state') v :=
+      let '(vs, _) := st' in set_contains vs v.
+
+    Fixpoint dfs_fold' n st' v :=
+      if already_seen st' v then backedge_upd' st' v else
+        match n with
+        | S n' => fold_left (dfs_fold' n') (edges v) (foreedge_upd' st' v)
+        | O => st'
+        end.
+
+    Definition dfs_fold := dfs_fold' (length (map.keys g)).
+  End fold.
+
+  Definition check_tree :=
+    dfs_fold (fun _ _ _ => false) (fun tree _ _ => true).
