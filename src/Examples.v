95b555c81d3b505fc4b1f0cc9e23118875bf8c74
Owen Conoly <otconoly@hotmail.com>
2026-08-07 18:40:39 -0400

    basically finish check_tree_spec, modulo incorrect definitions etc

--- src/Examples.v
+++ src/Examples.v
@@ -71,8 +71,8 @@ Section __.
       + intro x. rewrite In_list_union_spec, graph.all_nodes_put. cbn [In].
         rewrite In_list_union_spec. cbn [In].
         split.
-        * intros [Hr | [Hg | [Hxu | Hxv]]]; try (subst x); tauto.
-        * intros [Hxv | [Hr | Hg]]; try (subst x); tauto.
+        * intros [Hr | [Hg | [Hxu | Hxv]]]; subst; tauto.
+        * intros [Hxv | [Hr | Hg]]; subst; tauto.
     - reflexivity.
   Qed.
 
@@ -86,13 +86,11 @@ Section __.
     - apply list_union_preserves_NoDup. apply graph.all_nodes_NoDup.
     - intro x. rewrite !In_list_union_spec, graph.all_nodes_put. cbn [In] in *.
       split.
-      + intros [Hr | [Hg | [Hxu | Hxv]]]; try tauto.
-        * subst x. tauto.
-        * subst x. tauto.
+      + intros [Hr | [Hg | [Hxu | Hxv]]]; subst; tauto.
       + tauto.
   Qed.
 
-  Lemma dfs_check_invariant (root : V) vs is_tree p (g : graph) :
+  Lemma check_tree_spec' (root : V) vs is_tree p (g : graph) :
     dfs_fold_state (fun _ _ _ => false) (fun tree _ _ => tree) (fun tree _ _ => tree)
       root [root] true vs is_tree p g ->
     Reflects (S (num_edges g) = num_nodes g root) is_tree.
@@ -110,75 +108,21 @@ Section __.
       + intro. subst. auto.
     - apply set_contains_true in H1. constructor.
       rewrite num_edges_put by assumption. rewrite num_nodes_put00.
-      +
-
-      eapply Reflects_iff; [eassumption|].
-      specialize (IH0p1 u ltac:(simpl; eauto)). apply IH0p0 in IH0p1.
-      destruct IH0p1 as [IH0p1|IH0p1].
-      + subst.
-      apply IH0p0 in IH0p1.
-        destruct IH0p1; auto. subst.
-
-      ssplit.
-      + eapply Reflects_iff; [eassumption|].
-        apply locally_tree_put_unseen; try assumption.
-        * intros w Hw. apply HvA. apply graph.all_nodes_spec. exists w. right. exact Hw.
-        * intros w Hw. apply HvA. apply graph.all_nodes_spec. exists w. left. exact Hw.
-      + cbv [same_set] in *. intros a. specialize (IHp1 a). cbn [In] in IHp1 |- *.
-        rewrite graph.all_nodes_put. split.
-        * intros [Hr | [Hg | [Hu' | Hv']]].
-          -- right. apply (proj1 IHp1). left. exact Hr.
-          -- right. apply (proj1 IHp1). right. exact Hg.
-          -- subst a. right. exact Hu_in.
-          -- left. symmetry. exact Hv'.
-        * intros [Hv' | Hsvs].
-          -- right. right. right. symmetry. exact Hv'.
-          -- apply (proj2 IHp1) in Hsvs.
-             destruct Hsvs as [Hr | Hg]; [ left; exact Hr | right; left; exact Hg ].
-      + apply incl_cons; [ left; reflexivity | apply incl_tl; exact IHp2 ].
-      + intros y [<- | Hys].
-        * eapply reaches_step.
-          -- eapply reaches_weaken; [ | apply IHp3; exact Hu_in ].
-             intros a c Hac. apply graph.edge_put. left. exact Hac.
-          -- apply graph.edge_put. right. split; reflexivity.
-        * eapply reaches_weaken; [ | apply IHp3; exact Hys ].
-          intros a c Hac. apply graph.edge_put. left. exact Hac.
-    - destruct st as [svs sb]. cbn [fst snd untree_edge_upd'] in IH |- *.
-      cbn [already_seen] in Hseen. apply set_contains_true in Hseen. fwd.
-      ssplit.
-      + constructor. apply duplicate_edge_not_locally_tree.
-        * apply IHp3. apply IHp2. left. reflexivity.
-        * apply IHp3. exact Hseen.
-        * exact Hnew.
-      + cbv [same_set] in *. intros a. specialize (IHp1 a). cbn [In] in IHp1 |- *.
-        rewrite graph.all_nodes_put. split.
-        * intros [Hr | [Hg | [Hu' | Hv']]].
-          -- apply (proj1 IHp1). left. exact Hr.
-          -- apply (proj1 IHp1). right. exact Hg.
-          -- subst a. apply IHp2. left. reflexivity.
-          -- subst a. exact Hseen.
-        * intros Hsvs. apply (proj2 IHp1) in Hsvs.
-          destruct Hsvs as [Hr | Hg]; [ left; exact Hr | right; left; exact Hg ].
-      + exact IHp2.
-      + intros y Hys. eapply reaches_weaken; [ | apply IHp3; exact Hys ].
-        intros a c Hac. apply graph.edge_put. left. exact Hac.
-    - destruct st as [svs sb]. cbn [fst snd finish'] in IH |- *. fwd. ssplit.
-      + exact IHp0.
-      + exact IHp1.
-      + apply incl_cons_inv in IHp2. exact (proj2 IHp2).
-      + exact IHp3.
-  Qed.
+      + assert (num_nodes g root <= S (num_edges g)) by (clear IH0p3; admit).
+        lia.
+      + apply IH0p0. apply IH0p1. simpl. auto.
+      + apply IH0p0. assumption.
+    - assumption.
+  Admitted.
 
   Lemma check_tree_spec g root :
     Reflects (locally_tree (graph.edge g) root) (check_locally_tree g root).
   Proof.
-    cbv [check_locally_tree].
-    destruct (dfs_fold (fun _ _ _ => false) (fun tree _ _ => tree) (fun tree _ _ => tree) g true root)
-      as [l b] eqn:E.
+    cbv [check_locally_tree]. Tactics.destruct_one_match.
     apply dfs_fold_spec in E. fwd.
-    apply locally_tree_reachable_subgraph in Ep0.
-    eapply Reflects_iff; [ | exact Ep0 ].
-    simpl in Ep1. apply dfs_check_invariant in Ep1. exact (proj1 Ep1).
-  Qed.
+    apply check_tree_spec' in Ep1.
+    Print locally_tree.
+    About locally_tree.
+  Admitted.
 
 End __.
